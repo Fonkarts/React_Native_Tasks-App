8f9537a25826bc3e9f08a7e63255ab18c8854714
Fonkarts <sebastienhouchet@gmail.com>
2023-01-21 15:18:22 +0100

    :art: Create first layout

--- App.js
+++ App.js
@@ -1,20 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View style={styles.appContainer}>
+            <View style={styles.inputContainer}>
+                <TextInput
+                    style={styles.text}
+                    placeholder="Your course goal !"
+                />
+                <Button title="Add Goal" />
+            </View>
+            <View style={styles.goalsContainer}>
+                <Text>List of Goals : </Text>
+            </View>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    appContainer: {
+        flex: 1,
+        paddingTop: 50,
+        paddingHorizontal: 16,
+    },
+    inputContainer: {
+        flex: 1,
+        flexDirection: "row",
+        // Organizes elements around the main axis
+        justifyContent: "space-between",
+        // Organizes elements around the cross axis
+        alignItems: "center",
+        marginBottom: 24,
+        borderBottomWidth: 1,
+        borderBottomColor: "#cccccc",
+    },
+    text: {
+        borderWidth: 1,
+        borderColor: "#cccccc",
+        width: "70%",
+        marginRight: 8,
+        padding: 8,
+    },
+    goalsContainer: {
+        flex: 6,
+    },
 });
